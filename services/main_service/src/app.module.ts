6f72c23592c444dfb5813bba6c04388ee9b15ab9
Lacky227 <tishko.artyr@gmail.com>
2026-01-13 00:18:45 +0200

    feat: register Prisma Module

--- services/main_service/src/app.module.ts
+++ services/main_service/src/app.module.ts
@@ -1,31 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/module/users.module';
-import {TypeOrmModule} from "@nestjs/typeorm";
 import { TasksModule } from './tasks/tasks.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
       UsersModule,
       TasksModule,
-      ConfigModule.forRoot({
-        isGlobal: true,
-      }),
-
-      TypeOrmModule.forRootAsync({
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
-          type: 'postgres',
-          host: 'postgres',
-          port: 5432,
-          username: configService.get('POSTGRES_USER'),
-          password: configService.get('POSTGRES_PASSWORD'),
-          database: configService.get('POSTGRES_DB'),
-          autoLoadEntities: true,
-          synchronize: true,
-        }),
-      }),
+      PrismaModule,
   ],
 })
 export class AppModule {}
