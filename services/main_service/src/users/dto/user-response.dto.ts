7938ad796f525850051094c7aedae15df90fee20
Lacky227 <tishko.artyr@gmail.com>
2026-01-15 00:27:46 +0200

    refactor(users): make age field optional in UserResponseDto

--- services/main_service/src/users/dto/user-response.dto.ts
+++ services/main_service/src/users/dto/user-response.dto.ts
@@ -2,9 +2,9 @@
 export class UserResponseDto {
     username: string;
     email: string;
-    age: number;
+    age?: number;
 
-    constructor(username: string, email: string, age: number) {
+    constructor(username: string, email: string, age?: number) {
         this.username = username;
         this.email = email;
         this.age = age;
