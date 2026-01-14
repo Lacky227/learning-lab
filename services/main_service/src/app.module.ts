68ae3bfa4ab20f4ca863abe53034c5748919b28f
Lacky227 <tishko.artyr@gmail.com>
2026-01-14 23:11:14 +0200

    feat: setup global ConfigModule

--- services/main_service/src/app.module.ts
+++ services/main_service/src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/module/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
       UsersModule,
       TasksModule,
       PrismaModule,
+      ConfigModule.forRoot({
+          isGlobal: true,
+      }),
   ],
 })
 export class AppModule {}
