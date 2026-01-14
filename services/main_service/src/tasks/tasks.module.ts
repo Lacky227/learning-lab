c61e928aa95fad85843bbf4eb3d0a349e4b2abc6
Lacky227 <tishko.artyr@gmail.com>
2026-01-15 00:35:19 +0200

    refactor(tasks): remove TypeORM import

--- services/main_service/src/tasks/tasks.module.ts
+++ services/main_service/src/tasks/tasks.module.ts
@@ -2,14 +2,11 @@ import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { TaskRepository } from './task.repository';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from './entities/task.entity';
 import { AiService } from './ai.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Task]),
     HttpModule,
   ],
   controllers: [TasksController],
