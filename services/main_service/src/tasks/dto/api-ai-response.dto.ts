a9a55ab5297308af7391fcf505b03856e564b323
Lacky227 <tishko.artyr@gmail.com>
2026-01-15 13:42:07 +0200

    feat(tasks): add validation fields

--- services/main_service/src/tasks/dto/api-ai-response.dto.ts
+++ services/main_service/src/tasks/dto/api-ai-response.dto.ts
@@ -1,7 +1,10 @@
+import { IsEnum } from "class-validator";
 import { TaskCategory } from "../enums/task-category.enum";
 import { TaskPriority } from "../enums/task-priority.enum";
 
 export class ApiAiResponseDto {
+    @IsEnum(TaskPriority)
     priority: TaskPriority;
+    @IsEnum(TaskCategory)
     category: TaskCategory;
 }
