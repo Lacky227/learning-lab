c6b4c6fbb0335b491c9702e061487b137dcd4af5
Lacky227 <tishko.artyr@gmail.com>
2026-01-16 23:09:33 +0200

    feat(auth): implement user validation and JWT login logic

--- services/main_service/src/auth/auth.service.ts
+++ services/main_service/src/auth/auth.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/service/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService
+    ) {}
+
+    async validateUser(email: string, pass: string): Promise<any> {
+        const user = await this.usersService.getUserByEmail(email);
+        if (user && await require('bcrypt').compare(pass, user.password)) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+
+    async login(user: any) {
+        const payload = { email: user.email, sub: user.id };
+        return {
+            access_token: this.jwtService.sign(payload),
+        };
+    }
+}
