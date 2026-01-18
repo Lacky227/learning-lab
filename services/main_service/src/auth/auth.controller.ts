483c394ffdd4d2bb8b604f1131c8610559c908f4
Lacky227 <tishko.artyr@gmail.com>
2026-01-18 21:23:03 +0200

    feat(auth): add route login and use guard

--- services/main_service/src/auth/auth.controller.ts
+++ services/main_service/src/auth/auth.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
+import type { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Req() req: Request) {
+    return this.authService.login(req.user);
+  }
 }
