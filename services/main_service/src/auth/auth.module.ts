bd3b8053e3be35d151b42ee868245e7bd783013d
Lacky227 <tishko.artyr@gmail.com>
2026-01-20 01:03:40 +0200

    fix(auth): register PassportModule and LocalStrategy in AuthModule

--- services/main_service/src/auth/auth.module.ts
+++ services/main_service/src/auth/auth.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/module/users.module';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
+    UsersModule,
+    PassportModule,
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET || 'defaultSecret',
@@ -12,6 +17,6 @@ import { JwtModule } from '@nestjs/jwt';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
