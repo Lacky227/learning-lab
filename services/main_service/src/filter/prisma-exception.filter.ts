e4213c2584aa4c099f236f663304c83c81efb52b
Lacky227 <tishko.artyr@gmail.com>
2026-01-21 14:58:18 +0200

    feat(filter): implement PrismaExceptionFilter to handle DB specific errors

---
+++ services/main_service/src/filter/prisma-exception.filter.ts
@@ -0,0 +1,32 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpStatus } from "@nestjs/common";
+import { Response } from "express";
+import { Prisma } from "@prisma/client";
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaExceptionFilter implements ExceptionFilter{
+    catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost): void {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        let status = HttpStatus.BAD_REQUEST;
+        let message = 'Database error';
+
+        switch (exception.code) {
+            case 'P2002':
+                status = HttpStatus.CONFLICT;
+                message = `Unique constraint failed on the field: ${exception.meta?.target}`;
+                break;
+            case 'P2025':
+                message = `Record not found: ${exception.meta?.cause || ''}`;
+                status = HttpStatus.NOT_FOUND;
+                break;
+            default:
+                message = exception.message;
+        }
+
+        response.status(status).json({
+            statusCode: status,
+            timestamp: new Date().toISOString(),
+            message: message,
+        });
+    }
+}
