3043bc45531199d33e7c8b60da45ecae025afb44
Lacky227 <tishko.artyr@gmail.com>
2026-01-21 12:46:19 +0200

    refactor: use static factory methods for API responses

--- services/main_service/src/tasks/dto/api-response.dto.ts
+++ services/main_service/src/tasks/dto/api-response.dto.ts
@@ -1,6 +1,19 @@
-
 export class ApiResponseDto<T> {
     success: boolean;
-    message?: string;
+    message: string;
     data?: T;
+
+    constructor(success: boolean, message: string, data?: T) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    static success<T>(message: string, data?: T): ApiResponseDto<T> {
+        return new ApiResponseDto<T>(true, message, data);
+    }
+
+    static failure<T>(message: string, data?: T): ApiResponseDto<T> {
+        return new ApiResponseDto<T>(false, message, data);
+    }
 }
